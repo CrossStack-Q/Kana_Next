544572448ea1ee708ba43e8018cf9197208b590c
CrossStack-Q <crossstack@gmail.com>
2023-06-09 07:39:43 +0530

    Day_1

--- app/page.tsx
+++ app/page.tsx
@@ -23,8 +23,8 @@ export default function Home() {
 
           <Navbar />
           <HomePage/>
-          <DatePage />
-          <GuestPage/>
+          {/* <DatePage /> */}
+          {/* <GuestPage/> */}
 
         </main>
       </ThemeProvider>
